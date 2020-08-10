d7ee4c54dad2657b7a8f17ad5034a0038d3662ce
Joshua Agee <joshua.agee@protonmail.com>
2020-08-10 15:22:48 -0500

    add db models to app.py

--- venv/app.py
+++ venv/app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, g
 
 DEBUG = True
 PORT = 8000
 
+import models
+
+
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request();
+    """connect to the db before each request"""
+    g.db = models.db
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    """Close the db connection after each request"""
+    g.db.close()
+    return response
 
 @app.route('/')
 def index():
